a3882dee786700312f1585487526ab4134f7307d
Ebraheem Ijaz <ebraheemijaz913@gmail.com>
2019-08-18 02:34:52 -0700

    updated

--- app.py
+++ app.py
@@ -443,5 +443,5 @@ def test_disconnect():
 
 
 if __name__ =="__main__":
-    app.run(host= "0.0.0.0", debug=True ,port=80, threaded=True)
-    # app.run(host= "0.0.0.0",port=80, threaded=True)
+    # app.run(host= "0.0.0.0", debug=True ,port=80, threaded=True)
+    app.run(host= "0.0.0.0",port=80, threaded=True)
